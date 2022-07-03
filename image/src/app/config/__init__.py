86e321863b96a2088741549621f405070147258b
kubagrzelak <jakubgrzelak1@gmail.com>
2022-07-03 22:59:19 +0100

    Initial V1 Boilerplate

--- image/src/app/config/__init__.py
+++
@@ -1,5 +0,0 @@
-"""config files for the project"""
-from .application import APPLICATION
-from .http_codes import HTTP_CODES
-from .log import configure_logging
-from .weeve import WEEVE
--- image/src/app/config/log.py
+++
@@ -1,9 +0,0 @@
-"""logging configurations"""
-from logging import DEBUG, basicConfig
-
-
-def configure_logging():
-    basicConfig(
-        level=DEBUG,
-        format="{'levelname': '%(levelname)s', 'asctime': '%(asctime)s', 'name': '%(name)s', 'message': '%(message)s'}",
-    )
